4f20e7ea4af4ad34e1a9676c497e5e9bc52ca0c1
Túri Gábor <turi.gabor@gmail.com>
2013-12-03 08:43:48 +0100

    Kezdeti parameteratadas

--- template.js
+++ template.js
@@ -1,1 +1,15 @@
 /*jslint plusplus: true, regexp: true, sloppy: true, unparam: true */
+
+var JSTemplate = function (html) {
+    if (html instanceof Array) {
+        html = html.join('');
+    } else if (arguments.length > 1) {
+        html = Array.prototype.join.call(arguments, '');
+    }
+    this.html = html;
+};
+JSTemplate.prototype = {
+    fetch: function () {
+        return this.html;
+    }
+};
--- tests/template_test.js
+++ tests/template_test.js
@@ -1,2 +1,14 @@
 /*jslint plusplus: true, ass: true, sloppy: true */
 /*global describe, it, afterEach, expect, JSTemplate */
+
+describe('init', function () {
+    it('Kezdeti parameteratadas', function () {
+        var str = 'Szia vilag!', t;
+        t = new JSTemplate(str);
+        expect(t.fetch()).toEqual(str);
+        t = new JSTemplate(str, str);
+        expect(t.fetch()).toEqual(str + str);
+        t = new JSTemplate([str, str]);
+        expect(t.fetch()).toEqual(str + str);
+    });
+});
